3b425f634f8857147e751224b40eddea3252b8af
Kamil Molendys <kamil.molendys@karhoo.com>
2020-11-02 21:02:44 +0100

    working on deleting and new design

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,15 @@ import React, { useState } from "react";
 import "./App.css";
 import "fontsource-metropolis/all.css";
 import User from "./user";
+import {
+  Button,
+  FilePicker,
+  Heading,
+  Pane,
+  Spinner,
+  TrashIcon,
+  UploadIcon,
+} from "evergreen-ui";
 
 const user = new User();
 
@@ -11,6 +20,8 @@ function App() {
   const [authenticated, setAuthenticated] = useState(false);
   // const [errorMessage, setErrorMessage] = useState("");
   const [loading, setLoading] = useState(false);
+  const [uploading, setUploading] = useState(false);
+  const [deleting, setDeleting] = useState(false);
   const [displaySuccess, setDisplaySuccess] = useState(false);
   const [userImages, setUserImages] = useState([""]);
 
@@ -23,11 +34,10 @@ function App() {
     await user.Login(secret);
 
     const images = await user.GetImages();
-    console.log(images);
 
     let tempImages: string[] = [];
     images.forEach((img) => {
-      tempImages.push("https://siasky.net/" + img.skylink.replace("sia:", ""));
+      tempImages.push(img.skylink.replace("sia:", ""));
     });
 
     setUserImages(tempImages);
@@ -38,69 +48,111 @@ function App() {
 
   const handleUpload = async (event) => {
     event.preventDefault();
-    setLoading(true);
+    setUploading(true);
 
     if (imageFile) {
       const img = await user.AddImage(imageFile);
-      setUserImages(["https://siasky.net/" + img.replace("sia:", ""), ...userImages])
+      setUserImages([img.replace("sia:", ""), ...userImages]);
       setDisplaySuccess(true);
     }
 
-    setLoading(false);
+    setUploading(false);
   };
 
-  const onImageChange = e => {
-    const files: File[] = Array.from(e.target.files);
+  const onImageDelete = async (event) => {
+    const skylink = event.target.id;
+    setDeleting(true);
+    await user.DeleteImage(skylink);
+    const foundIndex = userImages.findIndex((img) => img.search(skylink) > -1);
+    if (foundIndex > -1) {
+      setUserImages([
+        ...userImages.slice(0, foundIndex),
+        ...userImages.slice(foundIndex + 1),
+      ]);
+    }
+    setDeleting(false);
+  };
 
+  const onImageChange = (files: FileList) => {
     if (files) {
       if (files[0]) {
         imageFile = files[0];
       }
     }
-  }
+  };
 
   return (
     <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+      {/* <img src={logo} className="App-logo" alt="logo" /> */}
 
-        <div className="container">
-          <h1>Instasia</h1>
-
-          {authenticated ? (
-            <div>
-              <label htmlFor="output">Upload an image</label>
-              <div className="flex">
-                <input
-                  type="file"
-                  onChange={onImageChange}
+      <Pane>
+        <Heading textAlign={"center"} size={900} marginTop="default">
+          Instasia
+        </Heading>
+        {authenticated ? (
+          <div>
+            <Pane elevation={3} marginTop={16} marginBottom={32} padding={32}>
+              <Heading size={500} marginBottom={16}>
+                Upload a memory
+              </Heading>
+              <Pane display="flex" alignItems="left">
+                <FilePicker
+                  multiple={false}
+                  width={350}
+                  marginBottom={8}
+                  onChange={(files) => onImageChange(files)}
+                  placeholder="Select the file here!"
                 />
-              </div>
-              <div className="mb-4">
-                <button
-                  type="button"
+                <Button
+                  height={32}
+                  isLoading={uploading}
                   onClick={handleUpload}
-                  disabled={loading}
+                  marginLeft={16}
+                  appearance="primary"
+                  intent="success"
+                  iconBefore={UploadIcon}
                 >
-                  {loading ? "Uploading..." : "Upload"}
-                </button>
-                {displaySuccess && (
-                  <span className="success-message">Uploaded with success!</span>
-                )}
-              </div>
-              <div>
-                {
-                  userImages ?
-                    userImages.map(
-                      (image, index) => <p key={index}><img width="300" key={index} src={image} alt="info"/ ></p>
-                    ) : <div></div>
-                }
-              </div>
-            </div>
-          ) : (
-              <form onSubmit={handleLogin}>
-                <div className="mb-2">
-                  {/* <label htmlFor="output">Nickname</label>
+                  Upload
+                </Button>
+              </Pane>
+            </Pane>
+
+            {userImages ? (
+              userImages.map((image, index) => {
+                let imageWithProtocol =
+                  "https://siasky.net/" + image.replace("sia:", "");
+                return (
+                  <Pane key={index} elevation={3} width={600} marginBottom={36}>
+                    <img
+                      width="100%"
+                      id={image}
+                      key={index}
+                      src={imageWithProtocol}
+                      alt="info"
+                    />
+                    <Button
+                      id={image}
+                      isLoading={deleting}
+                      margin={12}
+                      height={32}
+                      onClick={onImageDelete}
+                      appearance="minimal"
+                      intent="danger"
+                      iconBefore={TrashIcon}
+                    >
+                      Delete
+                    </Button>
+                  </Pane>
+                );
+              })
+            ) : (
+              <div></div>
+            )}
+          </div>
+        ) : (
+          <form onSubmit={handleLogin}>
+            <div className="mb-2">
+              {/* <label htmlFor="output">Nickname</label>
                 <div className="flex">
                   <input
                     id="output"
@@ -110,26 +162,25 @@ function App() {
                     onChange={(event) => setNickName(event.target.value)}
                   />
                 </div> */}
-                  <label htmlFor="output">Login passphrase</label>
-                  <div className="flex">
-                    <input
-                      id="output"
-                      type="secret"
-                      placeholder="Your very secret passphrase"
-                      value={secret}
-                      onChange={(event) => setSecret(event.target.value)}
-                    />
-                  </div>
-                </div>
-                <div className="mb-4">
-                  <button disabled={loading}>
-                    {loading ? "Logging..." : "Login"}
-                  </button>
-                </div>
-              </form>
-            )}
-        </div>
-      </header>
+              <label htmlFor="output">Login passphrase</label>
+              <div className="flex">
+                <input
+                  id="output"
+                  type="secret"
+                  placeholder="Your very secret passphrase"
+                  value={secret}
+                  onChange={(event) => setSecret(event.target.value)}
+                />
+              </div>
+            </div>
+            <div className="mb-4">
+              <button disabled={loading}>
+                {loading ? "Logging..." : "Login"}
+              </button>
+            </div>
+          </form>
+        )}
+      </Pane>
     </div>
   );
 }
--- src/dist/App.js
+++ src/dist/App.js
@@ -1,135 +1,382 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+var __awaiter =
+  (this && this.__awaiter) ||
+  function (thisArg, _arguments, P, generator) {
+    function adopt(value) {
+      return value instanceof P
+        ? value
+        : new P(function (resolve) {
+            resolve(value);
+          });
     }
-};
-var __spreadArrays = (this && this.__spreadArrays) || function () {
-    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
+    return new (P || (P = Promise))(function (resolve, reject) {
+      function fulfilled(value) {
+        try {
+          step(generator.next(value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function rejected(value) {
+        try {
+          step(generator["throw"](value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function step(result) {
+        result.done
+          ? resolve(result.value)
+          : adopt(result.value).then(fulfilled, rejected);
+      }
+      step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+  };
+var __generator =
+  (this && this.__generator) ||
+  function (thisArg, body) {
+    var _ = {
+        label: 0,
+        sent: function () {
+          if (t[0] & 1) throw t[1];
+          return t[1];
+        },
+        trys: [],
+        ops: [],
+      },
+      f,
+      y,
+      t,
+      g;
+    return (
+      (g = { next: verb(0), throw: verb(1), return: verb(2) }),
+      typeof Symbol === "function" &&
+        (g[Symbol.iterator] = function () {
+          return this;
+        }),
+      g
+    );
+    function verb(n) {
+      return function (v) {
+        return step([n, v]);
+      };
+    }
+    function step(op) {
+      if (f) throw new TypeError("Generator is already executing.");
+      while (_)
+        try {
+          if (
+            ((f = 1),
+            y &&
+              (t =
+                op[0] & 2
+                  ? y["return"]
+                  : op[0]
+                  ? y["throw"] || ((t = y["return"]) && t.call(y), 0)
+                  : y.next) &&
+              !(t = t.call(y, op[1])).done)
+          )
+            return t;
+          if (((y = 0), t)) op = [op[0] & 2, t.value];
+          switch (op[0]) {
+            case 0:
+            case 1:
+              t = op;
+              break;
+            case 4:
+              _.label++;
+              return { value: op[1], done: false };
+            case 5:
+              _.label++;
+              y = op[1];
+              op = [0];
+              continue;
+            case 7:
+              op = _.ops.pop();
+              _.trys.pop();
+              continue;
+            default:
+              if (
+                !((t = _.trys), (t = t.length > 0 && t[t.length - 1])) &&
+                (op[0] === 6 || op[0] === 2)
+              ) {
+                _ = 0;
+                continue;
+              }
+              if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) {
+                _.label = op[1];
+                break;
+              }
+              if (op[0] === 6 && _.label < t[1]) {
+                _.label = t[1];
+                t = op;
+                break;
+              }
+              if (t && _.label < t[2]) {
+                _.label = t[2];
+                _.ops.push(op);
+                break;
+              }
+              if (t[2]) _.ops.pop();
+              _.trys.pop();
+              continue;
+          }
+          op = body.call(thisArg, _);
+        } catch (e) {
+          op = [6, e];
+          y = 0;
+        } finally {
+          f = t = 0;
+        }
+      if (op[0] & 5) throw op[1];
+      return { value: op[0] ? op[1] : void 0, done: true };
+    }
+  };
+var __spreadArrays =
+  (this && this.__spreadArrays) ||
+  function () {
+    for (var s = 0, i = 0, il = arguments.length; i < il; i++)
+      s += arguments[i].length;
     for (var r = Array(s), k = 0, i = 0; i < il; i++)
-        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
-            r[k] = a[j];
+      for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
+        r[k] = a[j];
     return r;
-};
+  };
 exports.__esModule = true;
 var react_1 = require("react");
 // import logo from "./logo.svg";
 require("./App.css");
 require("fontsource-metropolis/all.css");
 var user_1 = require("./user");
+var evergreen_ui_1 = require("evergreen-ui");
 var user = new user_1["default"]();
 function App() {
-    var _this = this;
-    var _a = react_1.useState(""), secret = _a[0], setSecret = _a[1];
-    var _b = react_1.useState(false), authenticated = _b[0], setAuthenticated = _b[1];
-    // const [errorMessage, setErrorMessage] = useState("");
-    var _c = react_1.useState(false), loading = _c[0], setLoading = _c[1];
-    var _d = react_1.useState(false), displaySuccess = _d[0], setDisplaySuccess = _d[1];
-    var _e = react_1.useState([""]), userImages = _e[0], setUserImages = _e[1];
-    var imageFile;
-    var handleLogin = function (event) { return __awaiter(_this, void 0, void 0, function () {
-        var images, tempImages;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0:
-                    event.preventDefault();
-                    setLoading(true);
-                    return [4 /*yield*/, user.Login(secret)];
-                case 1:
-                    _a.sent();
-                    return [4 /*yield*/, user.GetImages()];
-                case 2:
-                    images = _a.sent();
-                    console.log(images);
-                    tempImages = [];
-                    images.forEach(function (img) {
-                        tempImages.push("https://siasky.net/" + img.skylink.replace("sia:", ""));
-                    });
-                    setUserImages(tempImages);
-                    setAuthenticated(true);
-                    setLoading(false);
-                    return [2 /*return*/];
-            }
-        });
-    }); };
-    var handleUpload = function (event) { return __awaiter(_this, void 0, void 0, function () {
-        var img;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0:
-                    event.preventDefault();
-                    setLoading(true);
-                    if (!imageFile) return [3 /*break*/, 2];
-                    return [4 /*yield*/, user.AddImage(imageFile)];
-                case 1:
-                    img = _a.sent();
-                    setUserImages(__spreadArrays(["https://siasky.net/" + img.replace("sia:", "")], userImages));
-                    setDisplaySuccess(true);
-                    _a.label = 2;
-                case 2:
-                    setLoading(false);
-                    return [2 /*return*/];
-            }
-        });
-    }); };
-    var onImageChange = function (e) {
-        var files = Array.from(e.target.files);
-        if (files) {
-            if (files[0]) {
-                imageFile = files[0];
-            }
+  var _this = this;
+  var _a = react_1.useState(""),
+    secret = _a[0],
+    setSecret = _a[1];
+  var _b = react_1.useState(false),
+    authenticated = _b[0],
+    setAuthenticated = _b[1];
+  // const [errorMessage, setErrorMessage] = useState("");
+  var _c = react_1.useState(false),
+    loading = _c[0],
+    setLoading = _c[1];
+  var _d = react_1.useState(false),
+    uploading = _d[0],
+    setUploading = _d[1];
+  var _e = react_1.useState(false),
+    deleting = _e[0],
+    setDeleting = _e[1];
+  var _f = react_1.useState(false),
+    displaySuccess = _f[0],
+    setDisplaySuccess = _f[1];
+  var _g = react_1.useState([""]),
+    userImages = _g[0],
+    setUserImages = _g[1];
+  var imageFile;
+  var handleLogin = function (event) {
+    return __awaiter(_this, void 0, void 0, function () {
+      var images, tempImages;
+      return __generator(this, function (_a) {
+        switch (_a.label) {
+          case 0:
+            event.preventDefault();
+            setLoading(true);
+            return [4 /*yield*/, user.Login(secret)];
+          case 1:
+            _a.sent();
+            return [4 /*yield*/, user.GetImages()];
+          case 2:
+            images = _a.sent();
+            tempImages = [];
+            images.forEach(function (img) {
+              tempImages.push(img.skylink.replace("sia:", ""));
+            });
+            setUserImages(tempImages);
+            setAuthenticated(true);
+            setLoading(false);
+            return [2 /*return*/];
         }
-    };
-    return (react_1["default"].createElement("div", { className: "App" },
-        react_1["default"].createElement("header", { className: "App-header" },
-            react_1["default"].createElement("div", { className: "container" },
-                react_1["default"].createElement("h1", null, "Instasia"),
-                authenticated ? (react_1["default"].createElement("div", null,
-                    react_1["default"].createElement("label", { htmlFor: "output" }, "Upload an image"),
-                    react_1["default"].createElement("div", { className: "flex" },
-                        react_1["default"].createElement("input", { type: "file", onChange: onImageChange })),
-                    react_1["default"].createElement("div", { className: "mb-4" },
-                        react_1["default"].createElement("button", { type: "button", onClick: handleUpload, disabled: loading }, loading ? "Uploading..." : "Upload"),
-                        displaySuccess && (react_1["default"].createElement("span", { className: "success-message" }, "Uploaded with success!"))),
-                    react_1["default"].createElement("div", null, userImages ?
-                        userImages.map(function (image, index) { return react_1["default"].createElement("p", { key: index },
-                            react_1["default"].createElement("img", { width: "300", key: index, src: image, alt: "info" })); }) : react_1["default"].createElement("div", null)))) : (react_1["default"].createElement("form", { onSubmit: handleLogin },
-                    react_1["default"].createElement("div", { className: "mb-2" },
-                        react_1["default"].createElement("label", { htmlFor: "output" }, "Login passphrase"),
-                        react_1["default"].createElement("div", { className: "flex" },
-                            react_1["default"].createElement("input", { id: "output", type: "secret", placeholder: "Your very secret passphrase", value: secret, onChange: function (event) { return setSecret(event.target.value); } }))),
-                    react_1["default"].createElement("div", { className: "mb-4" },
-                        react_1["default"].createElement("button", { disabled: loading }, loading ? "Logging..." : "Login"))))))));
+      });
+    });
+  };
+  var handleUpload = function (event) {
+    return __awaiter(_this, void 0, void 0, function () {
+      var img;
+      return __generator(this, function (_a) {
+        switch (_a.label) {
+          case 0:
+            event.preventDefault();
+            setUploading(true);
+            if (!imageFile) return [3 /*break*/, 2];
+            return [4 /*yield*/, user.AddImage(imageFile)];
+          case 1:
+            img = _a.sent();
+            setUserImages(
+              __spreadArrays([img.replace("sia:", "")], userImages)
+            );
+            setDisplaySuccess(true);
+            _a.label = 2;
+          case 2:
+            setUploading(false);
+            return [2 /*return*/];
+        }
+      });
+    });
+  };
+  var onImageDelete = function (event) {
+    return __awaiter(_this, void 0, void 0, function () {
+      var skylink, foundIndex;
+      return __generator(this, function (_a) {
+        switch (_a.label) {
+          case 0:
+            skylink = event.target.id;
+            setDeleting(true);
+            return [4 /*yield*/, user.DeleteImage(skylink)];
+          case 1:
+            _a.sent();
+            foundIndex = userImages.findIndex(function (img) {
+              return img.search(skylink) > -1;
+            });
+            if (foundIndex > -1) {
+              setUserImages(
+                __spreadArrays(
+                  userImages.slice(0, foundIndex),
+                  userImages.slice(foundIndex + 1)
+                )
+              );
+            }
+            setDeleting(false);
+            return [2 /*return*/];
+        }
+      });
+    });
+  };
+  var onImageChange = function (files) {
+    if (files) {
+      if (files[0]) {
+        imageFile = files[0];
+      }
+    }
+  };
+  return react_1["default"].createElement(
+    "div",
+    { className: "App" },
+    react_1["default"].createElement(
+      evergreen_ui_1.Pane,
+      null,
+      react_1["default"].createElement(
+        evergreen_ui_1.Heading,
+        { textAlign: "center", size: 900, marginTop: "default" },
+        "Instasia"
+      ),
+      authenticated
+        ? react_1["default"].createElement(
+            "div",
+            null,
+            react_1["default"].createElement(
+              evergreen_ui_1.Pane,
+              { elevation: 3, marginTop: 16, marginBottom: 32, padding: 32 },
+              react_1["default"].createElement(
+                evergreen_ui_1.Heading,
+                { size: 500, marginBottom: 16 },
+                "Upload a memory"
+              ),
+              react_1["default"].createElement(
+                evergreen_ui_1.Pane,
+                { display: "flex", alignItems: "left" },
+                react_1["default"].createElement(evergreen_ui_1.FilePicker, {
+                  multiple: false,
+                  width: 350,
+                  marginBottom: 8,
+                  onChange: function (files) {
+                    return onImageChange(files);
+                  },
+                  placeholder: "Select the file here!",
+                }),
+                react_1["default"].createElement(
+                  evergreen_ui_1.Button,
+                  {
+                    height: 32,
+                    isLoading: uploading,
+                    onClick: handleUpload,
+                    marginLeft: 16,
+                    appearance: "primary",
+                    intent: "success",
+                    iconBefore: evergreen_ui_1.UploadIcon,
+                  },
+                  "Upload"
+                )
+              )
+            ),
+            userImages
+              ? userImages.map(function (image, index) {
+                  var imageWithProtocol =
+                    "https://siasky.net/" + image.replace("sia:", "");
+                  return react_1["default"].createElement(
+                    evergreen_ui_1.Pane,
+                    { key: index, elevation: 3, width: 600, marginBottom: 36 },
+                    react_1["default"].createElement("img", {
+                      width: "100%",
+                      id: image,
+                      key: index,
+                      src: imageWithProtocol,
+                      alt: "info",
+                    }),
+                    react_1["default"].createElement(
+                      evergreen_ui_1.Button,
+                      {
+                        id: image,
+                        isLoading: deleting,
+                        margin: 12,
+                        height: 32,
+                        onClick: onImageDelete,
+                        appearance: "minimal",
+                        intent: "danger",
+                        iconBefore: evergreen_ui_1.TrashIcon,
+                      },
+                      "Delete"
+                    )
+                  );
+                })
+              : react_1["default"].createElement("div", null)
+          )
+        : react_1["default"].createElement(
+            "form",
+            { onSubmit: handleLogin },
+            react_1["default"].createElement(
+              "div",
+              { className: "mb-2" },
+              react_1["default"].createElement(
+                "label",
+                { htmlFor: "output" },
+                "Login passphrase"
+              ),
+              react_1["default"].createElement(
+                "div",
+                { className: "flex" },
+                react_1["default"].createElement("input", {
+                  id: "output",
+                  type: "secret",
+                  placeholder: "Your very secret passphrase",
+                  value: secret,
+                  onChange: function (event) {
+                    return setSecret(event.target.value);
+                  },
+                })
+              )
+            ),
+            react_1["default"].createElement(
+              "div",
+              { className: "mb-4" },
+              react_1["default"].createElement(
+                "button",
+                { disabled: loading },
+                loading ? "Logging..." : "Login"
+              )
+            )
+          )
+    )
+  );
 }
 exports["default"] = App;
