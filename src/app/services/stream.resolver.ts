31e866c198c50bec8a228add620c7dea244eeec2
Kamil Molendys <kamilmolendys@gmail.com>
2021-03-18 21:58:03 +0100

    stream fix

--- src/app/services/stream.resolver.ts
+++ src/app/services/stream.resolver.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { EMPTY, from, Observable, zip } from 'rxjs';
 import { map, catchError, first } from 'rxjs/operators';
+import { SKYBRAIN_ACCOUNT_PUBLIC_KEY } from '../models/user-connected-users';
 import { mapStreamMemoryToMemory, Memory } from '../reducers/memory/memory.model';
 import { ApiService } from './api.service';
 
@@ -11,18 +12,22 @@ import { ApiService } from './api.service';
 export class StreamResolver implements Resolve<{
   memories: Memory[],
 }> {
-  constructor(private apiService: ApiService, private router: Router) { }
+  constructor(
+    private apiService: ApiService,
+    private router: Router,
+    @Inject(SKYBRAIN_ACCOUNT_PUBLIC_KEY) private skyBrainAccountPublicKey: string) { }
 
   resolve(
     route: ActivatedRouteSnapshot,
     _: RouterStateSnapshot
   ): Observable<{ memories: Memory[] }> {
     return zip(
+      from(this.apiService.getBrainData({ publicKey: this.skyBrainAccountPublicKey })), // fix not displayed user names
       from(this.apiService.getStreamMemories()),
     )
       .pipe(
         first(),
-        map(([publicMemories]) => {
+        map(([brainData, publicMemories]) => {
           const memories = publicMemories.map(mapStreamMemoryToMemory);
           return { memories };
         }),
